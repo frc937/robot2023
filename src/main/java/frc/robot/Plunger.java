66e2733e33f8041c048ae3fd409fd21ac0764243
Phoebe <phoebe.g.shobe@gmail.com>
2023-01-14 10:47:01 -0600

    Made the plunger able to deploy using a PWMmotor and stop. - Phoebe

---
+++ src/main/java/frc/robot/Plunger.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Plunger {
+        //define motor + other things we'll need 
+        MotorController 
+
+
+    public Command deployPlunger() {
+        //code the deploys the Plunger
+
+    }
+}
