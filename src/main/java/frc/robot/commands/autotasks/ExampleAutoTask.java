b8ffb2241cfe8c3573d807c71a85e761247b9619
QuackitsQuinn <ollieollieausum@gmail.com>
2023-01-19 12:56:41 -0600

    Add base code to ExampleAutoTask.java
    - Quinn

---
+++ src/main/java/frc/robot/commands/autotasks/ExampleAutoTask.java
@@ -0,0 +1,14 @@
+package frc.robot.commands.autotasks;
+
+public class ExampleAutoTask extends AutoTask{
+    public void initTask(){}
+    public boolean initFinished(){
+        return true;
+    }
+    public void arrived(){}
+    public boolean arrivedFinished(){
+        return true;
+    }
+    public void fallback(Position positions){}
+    public void update(Position position){}
+}
