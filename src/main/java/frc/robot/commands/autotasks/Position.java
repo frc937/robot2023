beb156c0f555048501cdb93318d98690c8a1b19d
Dylan Polson <admin@red2fred2.com>
2023-01-19 08:04:45 -0600

    Make Position a subclass of Pose This is why branching off for so long is a bad idea. It makes code reuse difficult. Using a Pose subclass will make the chnge of origin math much much easier. We'll  need to do that math to locate an exact arm position in the world.
    --Dylan

--- src/main/java/frc/robot/commands/autotasks/Position.java
+++ src/main/java/frc/robot/commands/autotasks/Position.java
@@ -1,25 +1,44 @@
 package frc.robot.commands.autotasks;
 
-public class Position {
-    private double x;
-    private double y;
-    private double rot;
+import frc.robot.positioning.Pose;
 
+/**
+ * The position class that was written before is exactly the same as the Pose
+ * class written in another branch. Pose allows you to do horrible math easily.
+ * It's probably best to use for things like trying to get the arm to a certain
+ * place in the world.
+ */
+public class Position extends Pose {
     public Position(double x, double y, double rot){
-        this.x = x;
-        this.y = y;
-        this.rot = rot;
-    }
-    public double getX(){
-        return x;
-    }
-    public double getY(){
-        return y;
+        super(x, y, 0);
+        rotate(0, 0, rot);
     }
 
-    public double getRot(){
-        return rot;
+    public double getRot() {
+        return this.getZRot();
     }
-
-
 }
+
+// public class Position {
+//     private double x;
+//     private double y;
+//     private double rot;
+
+//     public Position(double x, double y, double rot){
+//         this.x = x;
+//         this.y = y;
+//         this.rot = rot;
+//     }
+//     public double getX(){
+//         return x;
+//     }
+//     public double getY(){
+//         return y;
+//     }
+
+//     public double getRot(){
+//         return rot;
+//     }
+
+
+// }
