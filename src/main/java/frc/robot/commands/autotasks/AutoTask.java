3ef8b5ad1de6f77c91d51cf6c3aec76fbb505764
QuackitsQuinn <ollieollieausum@gmail.com>
2023-01-19 10:39:48 -0600

    Add clearer javadocs and links
    -Quinn

--- src/main/java/frc/robot/commands/autotasks/AutoTask.java
+++ src/main/java/frc/robot/commands/autotasks/AutoTask.java
@@ -10,10 +10,9 @@ public abstract class AutoTask extends CommandBase {
   private boolean initialized = false;
   private Position taskPos;
   /** Creates a new AutoTask. <p>
-   * Dont forgot to use addRequirements() to add subsystem dependencies.
+   * Dont forgot to use {@link #addRequirements} to add subsystem dependencies.
    */
   public AutoTask() {
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   @Override
@@ -22,8 +21,8 @@ public abstract class AutoTask extends CommandBase {
   }
 /**
  * Ran when the command is started. Allow basic movements before the PositionSystem moves the robot. <p>
- * Make sure to put the taskPosition() method in init to set where the robot will go. <p>
- * Not running taskPosition() will cause a NullPointerException
+ * Make sure to put the {@link #taskPosition(Position) taskPosition} method in init to set where the robot will go. <p>
+ * Not running taskPosition will cause a NullPointerException.
  */
   public abstract void initTask(); 
   /**
@@ -47,7 +46,7 @@ public abstract class AutoTask extends CommandBase {
   public abstract void fallback(Position positions);
 
   /**
-   * <strong>DONT OVERRIDE.</strong> Override update instead.
+   * <strong>DONT OVERRIDE.</strong> Override update instead. Overriding this will cause the AutoTask to never start.
    */
   @Override
   public void execute() {
@@ -60,13 +59,13 @@ public abstract class AutoTask extends CommandBase {
     
   }
   /**
-   * Use instead of execute. Functions as execute but with 
-   * @param position is the current position of the robot when update is ran.
+   * Use instead of execute. Functions as execute but with a position arguemnt.
+   * @param position the current position of the robot when update is ran.
    */
   public abstract void update(Position position);
   /**
    * Sets the position the bot will go to for the task. <p>
-   * Not running this in initCommand <strong>will</strong> cause a NullPointerException
+   * Not running this in {@link #initTask initTask} <strong>will</strong> cause a NullPointerException
    * @param position The position the bot will go to for the AutoTask
    */
   protected void taskPosition(Position position){
