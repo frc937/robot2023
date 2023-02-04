da6a70a2b608b051bcdd7539f982576443d65168
QuackitsQuinn <ollieollieausum@gmail.com>
2023-02-04 09:35:39 -0600

    Add example autotask
    - Quinn

--- src/main/java/frc/robot/commands/autotasks/AutoTask.java
+++ src/main/java/frc/robot/commands/autotasks/AutoTask.java
@@ -1,6 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// the WPILib BSD license file in the root directory of this project. 
 
 package frc.robot.commands.autotasks;
 
@@ -9,7 +9,7 @@ import java.util.Stack;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.positioning.Pose;
-
+//TODO: Add fallback commands
 /**
  * Base class for autotasks.
  * If you want to create an autotask extend this class.
