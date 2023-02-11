ba6ae58866d02c5bfc073e2048a23c9d62e99aa0
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2023-02-11 13:47:43 -0600

    More Javadoc (surprise!) --Braden

--- src/main/java/frc/robot/positioning/Node.java
+++ src/main/java/frc/robot/positioning/Node.java
@@ -6,50 +6,57 @@ package frc.robot.positioning;
 
 /**
  * One of the points on the path planning gridmap.
- * @param x - x coordinate on the field 
- * @param y - y coordinate on the field
- * @param hValue - The Heuristic value, distance of each node from (starting point || end point (idk which)) as the crow flies
- * @param gValue - The g value, the distance between the current node and the start node
- * @param fValue - The f value, the total cost of the node
- * @param parent - the parent Node
  */
 public class Node {
-  /** Creates a new Node. */
-  public Node(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+    
+    /* The logic behind private/public is that some values may be used in other classes, while hValue is unlikely to be used. */
+    /** x coordinate on the field */
+    private int x;
+    /** y coordinate on the field */
+    private int y;
+    /** The Heuristic value, distance of each node from (starting point || end point (idk which)) as the crow flies */
+    public int hValue;
+    /** The g value, the distance between the current node and the start node */
+    public int gValue;
+    /** The f value, the total cost of the node */
+    public int fValue;
+    /** The parent node */
+    private Node parent;
+ 
+    /** Creates a new Node. Input should be in centimeters*/
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  /* The logic behind private/public is that some values may be used in other classes, while hValue is unlikely to be used. */
-  private int x;
-  private int y;
-  public int hValue;
-  public int gValue;
-  public int fValue;
-  private Node parent;
+    /** Sets the Node's x value */
+    public void setX(int x) {
+        this.x = x;
+    }
 
-  public void setX(int x) {
-    this.x = x;
-  }
+    /** Sets the Node's y value */
+    public void setY(int y) {
+        this.y = y;
+    }
 
-  public void setY(int y) {
-    this.y = y;
-  }
+    /** Sets the Node's parent */
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
 
-  public void setParent(Node parent) {
-    this.parent = parent;
-  }
+    /** @return The Node's x value */
+    public int getX() {
+        return this.x;
+    }
 
-  public int getX() {
-    return this.x;
-  }
+    /** @return The Node's y value */
+    public int getY() {
+        return this.y;
+    }
 
-  public int getY() {
-    return this.y;
-  }
-
-  public Node getParent() {
-    return this.parent;
-  }
+    /** @return The Node's parent */
+    public Node getParent() {
+        return this.parent;
+    }
 
 }
