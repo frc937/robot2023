ffa7ee39747d18f880c92cc7a01cf004eadff3ad
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2023-02-11 12:51:25 -0600

    trajectory generator
    utility class to generate trajectories from the Path generated by A*
    
    --Gabe

--- src/main/java/frc/robot/AStarTrajectoryGenerator.java
+++ src/main/java/frc/robot/AStarTrajectoryGenerator.java
@@ -4,9 +4,33 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import frc.robot.positioning.Path;
+
+import java.util.ArrayList;
 
 /** Add your docs here. */
 public class AStarTrajectoryGenerator {
-    public static Trajectory generateTrajectory()
+    public static Trajectory generateTrajectory(Path path) {
+        ArrayList<Double[]> pathList = path.getPathList();
+
+        /* stealing max accel and velocity from holonomicController */
+        TrajectoryConfig config = new TrajectoryConfig(Constants.Drive.HolonomicController.ThetaController.Constraints.MAX_VELOCITY, Constants.Drive.HolonomicController.ThetaController.Constraints.MAX_ACCELERATION);
+
+        /* I *THINK* that we don't need to care about rotation because the HolonomicController calls will do that separately */
+        Pose2d start = new Pose2d(pathList.get(0)[0], pathList.get(0)[1], Rotation2d.fromDegrees(0));
+        Pose2d end = new Pose2d(pathList.get(pathList.size())[0], pathList.get(pathList.size())[1], Rotation2d.fromDegrees(0));
+
+        ArrayList<Translation2d> waypoints = new ArrayList<Translation2d>();
+        for (Double[] coordinate : pathList) {
+            waypoints.add(new Translation2d(coordinate[0], coordinate[1]));
+        }
+
+        return TrajectoryGenerator.generateTrajectory(start, waypoints, end, config);
+    }
 }
