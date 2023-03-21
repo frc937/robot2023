23ed3e0827e57c72aa7f985b88591e25655844eb
berdenson <braden.reighard@icloud.com>
2023-03-21 15:23:48 -0600

    Added charging station.java file --Braden

---
+++ src/main/java/frc/robot/commands/autotasks/ChargingStation.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.autotasks;
+
+import frc.robot.positioning.Pose;
+
+public class ChargingStation extends AutoTask {
+
+  @Override
+  public void initTask() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void arrived() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void fallback(Pose position) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void update(Pose position) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+
+
+}
