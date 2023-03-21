ab172af0665a38d56b3a8823cdcfa44ab3120444
berdenson <braden.reighard@icloud.com>
2023-03-21 14:58:18 -0600

    mobility bonus mostly finished, needs input from shuffleboard and actual parameters --Braden

--- src/main/java/frc/robot/commands/autotasks/MobilityBonus.java
+++ src/main/java/frc/robot/commands/autotasks/MobilityBonus.java
@@ -3,6 +3,7 @@ package frc.robot.commands.autotasks;
 import frc.robot.positioning.Pose;
 import frc.robot.positioning.UnknownPose;
 import frc.robot.positioning.AStar;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 
 public class MobilityBonus extends AutoTask {
@@ -10,8 +11,15 @@ public class MobilityBonus extends AutoTask {
   @Override
   public void initTask() {
     // TODO Auto-generated method stub
-    setTaskPosition(new UnknownPose());
-    generateTaskPath();
+
+    // TODO Get boolean input from shuffleboard, for top or bottom exit
+    if (false) {
+      setTaskPosition(new UnknownPose());
+    }
+
+    else {
+      setTaskPosition(new UnknownPose());
+    }
   }
 
   @Override
@@ -29,9 +37,7 @@ public class MobilityBonus extends AutoTask {
   @Override
   protected void update(Pose position) {
     // TODO Auto-generated method stub
-    if (isPathGenerated()) {
-      State = intialized;
-    }
+
   }
   
 }
