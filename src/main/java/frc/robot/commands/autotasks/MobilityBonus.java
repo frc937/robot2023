2f54b5f971d571c9265777957132a9feb086f341
berdenson <braden.reighard@icloud.com>
2023-03-22 17:07:50 -0600

    Made to choose alliance's sides --Braden

--- src/main/java/frc/robot/commands/autotasks/MobilityBonus.java
+++ src/main/java/frc/robot/commands/autotasks/MobilityBonus.java
@@ -3,6 +3,7 @@ package frc.robot.commands.autotasks;
 import frc.robot.positioning.Pose;
 import frc.robot.positioning.UnknownPose;
 import frc.robot.positioning.AStar;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
@@ -17,16 +18,33 @@ public class MobilityBonus extends AutoTask {
 
     isTopExit = SmartDashboard.getBoolean("Exit from Top?", true);
 
-    // TODO Get boolean input from shuffleboard, for top or bottom exit
-    if (isTopExit) {
-      // make sure this one is the top exit V
-      setTaskPosition(new UnknownPose());
+    if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
+      if (isTopExit) {
+        // make sure this one is the top exit V
+        setTaskPosition(new UnknownPose());
+      }
+  
+      else {
+        setTaskPosition(new UnknownPose());
+      }
     }
 
-    else {
-      setTaskPosition(new UnknownPose());
+    else if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
+      if (isTopExit) {
+        // make sure this one is the top exit V
+        setTaskPosition(new UnknownPose());
+      }
+  
+      else {
+        setTaskPosition(new UnknownPose());
+      }
     }
-  }
+    else {
+      //help
+    }
+
+    }
+    
 
   @Override
   public void arrived() {
