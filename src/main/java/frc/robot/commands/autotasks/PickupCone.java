941559e64c53dfab057b8f7bf797529984312b76
berdenson <braden.reighard@icloud.com>
2023-03-22 14:46:58 -0600

    made PickupCone.java --Braden (i'm cool)

---
+++ src/main/java/frc/robot/commands/autotasks/PickupCone.java
@@ -0,0 +1,31 @@
+package frc.robot.commands.autotasks;
+
+import frc.robot.positioning.Pose;
+
+public class PickupCone extends AutoTask {
+
+  @Override
+  public void initTask() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void arrived() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void fallback(Pose position) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  protected void update(Pose position) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
