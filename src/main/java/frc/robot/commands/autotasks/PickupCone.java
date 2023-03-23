7661021275e7a301dc36159bfd719624adc2fc64
berdenson <braden.reighard@icloud.com>
2023-03-23 14:04:05 -0600

    PickupCone changes on cone location --Braden

--- src/main/java/frc/robot/commands/autotasks/PickupCone.java
+++ src/main/java/frc/robot/commands/autotasks/PickupCone.java
@@ -1,19 +1,65 @@
 package frc.robot.commands.autotasks;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.positioning.Pose;
 import frc.robot.positioning.UnknownPose;
 
 public class PickupCone extends AutoTask {
 
+  private enum Spot {
+    TOP,
+    MIDDLE,
+    BOTTOM
+  };
+  private Spot spot;
+  private boolean isTop;
+  private boolean isMiddle;
+  private boolean isBottom;
+
   @Override
   public void initTask() {
-    // TODO Auto-generated method stub
-    if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
-      setTaskPosition(new UnknownPose());
+    
+    isTop = SmartDashboard.getBoolean("exitTop", false);
+    isMiddle = SmartDashboard.getBoolean("exitMiddle", false);
+    isBottom = SmartDashboard.getBoolean("exitBottom", false);
+
+    if (isTop) {
+      spot = Spot.TOP;
     }
-    else if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
-      setTaskPosition(new UnknownPose());
+    else if (isMiddle) {
+      spot = Spot.MIDDLE;
+    }
+    else if (isBottom) {
+      spot = Spot.BOTTOM;
+    }
+
+    if (spot == Spot.TOP) {
+
+      if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
+        setTaskPosition(new UnknownPose());
+      }
+    }
+    else if (spot == Spot.MIDDLE) {
+
+      if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
+        setTaskPosition(new UnknownPose());
+      }
+    }
+    else if (spot == Spot.BOTTOM) {
+      
+      if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
+        setTaskPosition(new UnknownPose());
+      }
     }
 
     // put fancy command here that does fancy pickup
