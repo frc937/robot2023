b79e14884457c70c278d21e5d8f600c342957734
berdenson <braden.reighard@icloud.com>
2023-03-23 16:20:13 -0600

    Places game piece based on type, location, alliance --Braden

--- src/main/java/frc/robot/commands/autotasks/PlaceGamePiece.java
+++ src/main/java/frc/robot/commands/autotasks/PlaceGamePiece.java
@@ -1,19 +1,89 @@
 package frc.robot.commands.autotasks;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.MoveToPose;
 import frc.robot.positioning.Pose;
+import frc.robot.positioning.UnknownPose;
 
 public class PlaceGamePiece extends AutoTask {
   private MoveToPose moveToPose;
+  /** Whether or not the object being picked up is a cone; false for cube */
+  private boolean isCone;
+  /** the location to place the cone at, starting from the top of the field */
+  private int location;
   public PlaceGamePiece(MoveToPose mToPose) {
     moveToPose = mToPose;
   }
 
   @Override
   public void initTask() {
-    //TODO
 
+    location = (int) SmartDashboard.getNumber("location", 1);
+    if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
+      if (location == 1) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 2) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 3) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 4) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 5) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 6) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 7) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 8) {
+        setTaskPosition(new UnknownPose());
+      }
+    }
+
+    if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) {
+      if (location == 1) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 2) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 3) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 4) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 5) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 6) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 7) {
+        setTaskPosition(new UnknownPose());
+      }
+      else if (location == 8) {
+        setTaskPosition(new UnknownPose());
+      }
+    }
+
+    isCone = SmartDashboard.getBoolean("isCone", true);
+
+    if (isCone) {
+      //put fancy cone command here
+    }
+    else {
+      //put fancy cube command here
+    }
+
+    // put fancy command here that does fancy pickup
   }
 
   @Override
