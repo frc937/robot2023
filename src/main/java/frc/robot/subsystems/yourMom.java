0d2cc80a87c44d61ccaa8bc440df19436189e492
Phoebe <phoebe.g.shobe@gmail.com>
2023-03-25 17:55:24 -0500

    fixed YourMom print command
    --Gabe

--- src/main/java/frc/robot/subsystems/yourMom.java
+++ src/main/java/frc/robot/subsystems/yourMom.java
@@ -5,13 +5,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 
 public class yourMom extends CommandBase {
   /** Creates a new yourMom. */
   public yourMom() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
+  /**
+   * prints "Your Mom"
+   */
+  public void printYourMom(){
+    System.out.print("Your Mom"); 
+  }
 
+  /**
+   * command to print "Your Mom"
+   * @return command that prints "Your Mom"
+   */
+  public PrintCommand commandPrintYourMom(){
+    return new PrintCommand("Your Mom"); 
+  }
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
