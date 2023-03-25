dcb61670fc9a866eff03795df8f4e017f2adfa91
Berdenson <braden.reighard@icloud.com>
2023-03-25 15:28:18 -0500

    split leavecommunity into two files for starting and stopping the motors --Braden

--- src/main/java/frc/robot/commands/StopLeavingCommunity.java
+++ src/main/java/frc/robot/commands/StopLeavingCommunity.java
@@ -5,16 +5,25 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
 
-public class LeaveCommunity extends CommandBase {
+/** Leaves the community for the  */
+public class StopLeavingCommunity extends CommandBase {
+
+  private final Drive drivetrain;
+
   /** Creates a new LeaveCommunity. */
-  public LeaveCommunity() {
+  public StopLeavingCommunity(Drive driveSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = driveSubsystem;
+    addRequirements(driveSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    drivetrain.stop();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -27,6 +36,6 @@ public class LeaveCommunity extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
