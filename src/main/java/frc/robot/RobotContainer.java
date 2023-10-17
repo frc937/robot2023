abe2a38988d3a66f48bfc020efd775c2e697af21
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2023-10-17 14:03:54 -0700

    Added back Asimov's Laws

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+ * Asimov's Three Laws of Robotics:
+ * 1: A robot may not injure a human being or, through inaction, allow a human being to come to harm.
+ * 2: A robot must obey the orders given to it by human beings except where such orders would conflict with the First Law.
+ * 3: A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
+ */
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
