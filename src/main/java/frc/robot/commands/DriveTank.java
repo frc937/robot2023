64bb3d70eb3194b17a656c5af523b4f8d172ef1c
notaroboticsenthusiast <91231142+notaroboticsenthusiast@users.noreply.github.com>
2023-11-01 18:36:09 -0500

    fix embarrassing typo
    --Gabe

--- src/main/java/frc/robot/commands/DriveTank.java
+++ src/main/java/frc/robot/commands/DriveTank.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 /*
- * Asmiov's Laws:
+ * Asimov's Laws:
  * The First Law: A robot may not injure a human being or, through inaction, allow a human being to come to harm.
  * The Second Law: A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.
  * The Third Law: A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
